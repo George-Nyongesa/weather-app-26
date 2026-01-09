a1044b73753f587658b46362f69504b1ef99b0ef
George Nyongesa <georgenyongesa254@gmail.com>
2026-01-09 17:45:59 +0300

    Initial commit: Weather app (frontend + backend)

---
+++ weather-backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/weather', async (req, res) => { 
+  const city = req.query.city;
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+
+  if (!city && (!lat || !lon)) {
+      return res.status(400).json({ error: 'City or coordinates are required' });
+  }
+
+  try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+          params: city
+            ? { q: city, appid: process.env.OPEN_WEATHER_API_KEY, units: 'metric' }
+            : { lat, lon, appid: process.env.OPEN_WEATHER_API_KEY, units: 'metric' },
+      });
+
+      res.json(response.data);
+  } catch (error) {
+      console.error(error);
+      res.status(404).json({ error: 'City not found' });
+  }
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
